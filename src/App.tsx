da361a07979f93537c13cdeade25c3084c4698ed
fancunshuo <fancunshuo@haier.com>
2024-08-16 16:27:38 +0800

    feat: add mdx editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  imagePlugin,
+  tablePlugin,
+  diffSourcePlugin,
+  linkDialogPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  BlockTypeSelect,
+  ListsToggle,
+  Separator,
+  InsertImage,
+  InsertTable,
+  CreateLink,
+  CodeToggle,
+  InsertCodeBlock,
+  InsertThematicBreak,
+  DiffSourceToggleWrapper,
+  StrikeThroughSupSubToggles,
+  ConditionalContents,
+  ChangeCodeMirrorLanguage,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  linkPlugin,
+} from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import markdown from '!!raw-loader!./assets/live-demo-contents.md'
+// import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MDXEditor
+      autoFocus={{ defaultSelection: 'rootEnd' }}
+      markdown={markdown}
+      plugins={[
+        toolbarPlugin({
+          toolbarContents: () => (
+            <DiffSourceToggleWrapper>
+              <ConditionalContents
+                options={[
+                  {
+                    when: (editor) => editor?.editorType === 'codeblock',
+                    contents: () => <ChangeCodeMirrorLanguage />,
+                  },
+
+                  {
+                    fallback: () => (
+                      <>
+                        {' '}
+                        <UndoRedo />
+                        <Separator />
+                        <BoldItalicUnderlineToggles
+                          options={['Bold', 'Italic']}
+                        />
+                        <CodeToggle />
+                        <Separator />
+                        <StrikeThroughSupSubToggles
+                          options={['Strikethrough']}
+                        />
+                        <Separator />
+                        <ListsToggle />
+                        <Separator />
+                        <BlockTypeSelect />
+                        <Separator />
+                        <CreateLink />
+                        <InsertImage />
+                        <Separator />
+                        <InsertTable />
+                        <InsertThematicBreak />
+                        <Separator />
+                        <InsertCodeBlock />
+                      </>
+                    ),
+                  },
+                ]}
+              ></ConditionalContents>
+            </DiffSourceToggleWrapper>
+          ),
+        }),
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        imagePlugin({
+          imageAutocompleteSuggestions: [
+            'https://via.placeholder.com/150',
+            'https://via.placeholder.com/150',
+          ],
+          imageUploadHandler: async () =>
+            Promise.resolve('https://picsum.photos/200/300'),
+        }),
+        tablePlugin(),
+        diffSourcePlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        codeBlockPlugin({
+          defaultCodeBlockLanguage: 'txt',
+          codeBlockEditorDescriptors: [],
+        }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: 'JavaScript',
+            css: 'CSS',
+            txt: 'Plain Text',
+            tsx: 'TypeScript',
+            '': 'Unspecified',
+          },
+        }),
+      ]}
+    />
+  )
 }
 
-export default App;
+export default App
