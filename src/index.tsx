4e30d4c0c6ed90205e2ce47078bf4fba60702d62
firstmetcs <824311265@qq.com>
2024-08-18 12:23:15 +0800

    feat: add dependency trying to solve error on safari

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// @ts-ignore
+import R from 'core-js/stable';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+R;
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
